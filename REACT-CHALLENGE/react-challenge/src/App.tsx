9ad31c0935304a9070089297e95f5c53defaa3ce
sofialzira <design.sofiaoliveira@gmail.com>
2024-08-25 17:32:41 +0100

    implemented bio component

--- REACT-CHALLENGE/react-challenge/src/App.tsx
+++ REACT-CHALLENGE/react-challenge/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import BioContainer from './components/BioContainer/BioContainer'
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
 
   return (
+    <>
      <NavBar />
+     <div>
+      <BioContainer 
+        bioJob="Web Designer"
+        bioName="Sofia Oliveira."
+        bioDescription="Escrever aqui bio bonitinha"
+      />
+     </div>
+
+     </>
   )
   
   }
